d0fef9f61603522ebb36a316699108faac8754d8
Chakraborty-Joyee <joyeechakraborty2000@gmail.com>
2023-05-27 15:50:49 +0600

    navbar added

---
+++ Navbar.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LU_Finder</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+        <div class="container">
+
+            <a class="navbar-brand me-2" href="https://mdbgo.com/">
+                <img src="../LU_Finder/Images/logo.jpg" height="50" alt="" loading="lazy" style="margin-top: -1px;" />
+            </a>
+            <h3 class="text-success m-auto  ">LU Finder</h3>
+
+            <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarButtonsExample" aria-controls="navbarButtonsExample" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars"></i>
+            </button>
+
+
+            <div class="collapse navbar-collapse" id="navbarButtonsExample">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                </ul>
+                <div class="d-flex align-items-center">
+                    <button type="submit" class="btn btn-success me-3">Login</button>
+                    <button type="submit" class="btn btn-success me-3">Register</button>
+
+                </div>
+            </div>
+
+        </div>
+
+    </nav>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
